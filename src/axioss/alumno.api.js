c70239cbfa636625663afb44186918d466a71883
Rodrigo <rodrigo695@hotmail.com>
2023-01-10 17:14:45 -0300

    2

--- src/axioss/alumno.api.js
+++ src/axioss/alumno.api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const getAlumnosRq = async () =>
-    await axios.get("http://localhost:8080/alumnos");
+    await axios.get("https://pwaback-production-23e5.up.railway.app/alumnos");
 
 export const createAlumnoRq = async (alumno) =>
-    await axios.post("http://localhost:8080/alumnos", alumno);
+    await axios.post("https://pwaback-production-23e5.up.railway.app/alumnos", alumno);
 
 export const deleteAlumnoRq = async (id) =>
-    await axios.delete(`http://localhost:8080/alumnos/${id}`);
+    await axios.delete(`https://pwaback-production-23e5.up.railway.app/alumnos/${id}`);
 
 export const getAlumnoRq = async (id) =>
-    await axios.get(`http://localhost:8080/alumnos/${id}`);
+    await axios.get(`https://pwaback-production-23e5.up.railway.app/alumnos/${id}`);
 
